e4f49510398db92fbf28be8de21b889427d54323
SANDAY-JS <san.sh.s3@outlook.jp>
2022-06-21 10:18:21 +0900

    Changed VoiceItem Design

--- src/components/VoiceItem.tsx
+++ src/components/VoiceItem.tsx
@@ -19,16 +19,15 @@ function VoiceItem({ name, link, src, des }) {
   }, []);
 
   return (
-    <div ref={elRef} className={`flex flex-col items-center gap-3 bg-[#fff] rounded-lg px-4 lg:px-8 pt-2 pb-6 w-full max-w-xl lg:flex-1 lg:w-1/4 transition-opacity duration-500 opacity-0 ${visible && "opacity-100"}`}>
-      <div className="flex justify-center items-center gap-1 w-4/5 sm:flex-col sm:gap-3">
+    <div ref={elRef} className={`flex flex-col items-center justify-between gap-3 bg-[#fff] rounded-lg px-8 pt-4 pb-2 w-full max-w-xl lg:flex-1 lg:w-1/4 transition-opacity duration-500 opacity-0 ${visible && "opacity-100"}`}>
+      <p className="text-xl text-center">"{des}"</p>
+
+      <div className="flex justify-center items-center gap-1">
         <Image src={src} width={66} height={66} layout="intrinsic" objectFit="cover" className="max-w-sm rounded-full w-6 h-6"/>
         <h3 className="text-mainB text-center text-md sm:text-lg md:text-xl font-extrabold">
-          <Link href={link}>
-            <a className="hover:underline">{name}様</a>
-          </Link>
+          <Link href={link}><a className="underline whitespace-nowrap">{name}様</a></Link>
         </h3>
       </div>
-      <p className="text-md sm:text-lg md:text-xl text-center">{des}</p>
     </div>
   );
 }
