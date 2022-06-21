2dd5a8916fc5ea391b5975231d68f5807a64d179
SANDAY-JS <san.sh.s3@outlook.jp>
2022-06-21 12:18:04 +0900

    Changed Payment design

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -6,48 +6,38 @@ function Payment() {
     <section className="relative bg-web-person bg-left md:bg-center bg-no-repeat overflow-x-hidden pb-6 xl:mx-auto xl:max-w-screen-lg">
       <h2 id="to_payment">Payment</h2>
       <p>料金</p>
-      <div className="flex flex-col items-center gap-5 2xl:max-w-screen-xl 2xl:mx-auto z-10">
+      <div className="flex flex-col md:flex-row justify-center items-center flex-wrap gap-5 2xl:max-w-screen-xl 2xl:mx-auto z-10">
         <PaymentItem
           title={"ホームページ制作"}
           matter1={"Webページ / LP制作"}
           payment1={"69,000"}
-          payment1Des={
-            "（通常料金：¥59,000 + レスポンシブ化：¥10,000）＊参考サイト・デザインをご提示ください"
-          }
           matter2={null}
           payment2={null}
-          payment2Des={null}
-          num={false}
+          index={0}
         />
         <PaymentItem
           title={"Webシステム開発"}
           matter1={"フロントエンド開発"}
           payment1={"39,000"}
-          payment1Des={""}
           matter2={null}
           payment2={null}
-          payment2Des={null}
-          num={true}
+          index={1}
         />
         <PaymentItem
           title={"アニメーション制作"}
           matter1={"動画制作"}
           payment1={"29,000"}
-          payment1Des={"＊～30秒の場合。長さによって料金が前後いたします。"}
-          matter2={"サイト内のアニメーション再現"}
+          matter2={"サイト内アニメーション"}
           payment2={"19,000"}
-          payment2Des={""}
-          num={false}
+          index={2}
         />
         <PaymentItem
           title={"WordPress開発"}
-          matter1={"デザイン, フロントエンド開発"}
+          matter1={"デザインの変更・修正"}
           payment1={"39,000"}
-          payment1Des={""}
-          matter2={null}
-          payment2={null}
-          payment2Des={null}
-          num={true}
+          matter2={"機能の追加(システム開発)"}
+          payment2={"39,000"}
+          index={3}
         />
       </div>
     </section>
