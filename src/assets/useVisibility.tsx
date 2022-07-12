528b6e78c3760ba6b52c4cc9e4140fd3c2110a9f
SANDAY-JS <san.sh.s3@outlook.jp>
2022-07-12 12:25:15 +0900

    Fixed TypeScript Error

--- src/assets/useVisibility.tsx
+++ src/assets/useVisibility.tsx
@@ -7,9 +7,9 @@ import throttle from "lodash.throttle";
  * @param {number} offset - Number of pixels up to the observable element from the top
  * @param {number} throttleMilliseconds - Throttle observable listener, in ms
  */
-function useVisibility( offset = -200, throttleMilliseconds = 100, afterEffect = false) {
+function useVisibility<Element extends HTMLElement>( offset = -200, throttleMilliseconds = 100, afterEffect = false): [Boolean, React.LegacyRef<HTMLDivElement>] {
   const [isVisible, setIsVisible] = useState(false);
-  const currentElement = useRef();
+  const currentElement = useRef<HTMLDivElement>();
   const topMinusOffset = afterEffect ? -1200 : 0;
 
   const handleScroll = throttle(() => {
