265fe6b52d2fb021e8b97728219ca761e91fbf7b
SANDAY-JS <san.sh.s3@outlook.jp>
2022-07-12 12:34:55 +0900

    Fixed Build Error

--- src/assets/useVisibility.tsx
+++ src/assets/useVisibility.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import throttle from "lodash.throttle";
 
 /**
@@ -7,14 +7,13 @@ import throttle from "lodash.throttle";
  * @param {number} offset - Number of pixels up to the observable element from the top
  * @param {number} throttleMilliseconds - Throttle observable listener, in ms
  */
-function useVisibility<Element extends HTMLElement>( offset = -200, throttleMilliseconds = 100, afterEffect = false): [Boolean, React.LegacyRef<HTMLDivElement>] {
+function useVisibility( offset = -200, throttleMilliseconds = 100, afterEffect = false): [Boolean, React.RefObject<any>] {
   const [isVisible, setIsVisible] = useState(false);
-  const currentElement = useRef<HTMLDivElement>();
+  const currentElement = useRef<HTMLElement>(null);
   const topMinusOffset = afterEffect ? -1200 : 0;
 
   const handleScroll = throttle(() => {
     if (!currentElement.current) {
-      console.log('out of question')
       setIsVisible(false);
       return;
     }
