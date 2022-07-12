7b238871a904780e5b43ea6db5d2779e33680d38
SANDAY-JS <san.sh.s3@outlook.jp>
2022-07-12 11:35:27 +0900

    Added useVisibility inAbout

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,38 +1,22 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { MenuListProvider } from "../provider/StateProvider";
 import gsap from "gsap";
+import useVisibility from "../assets/useVisibility";
 
 function About() {
-  const {
-    svgList,
-    mySkillsList,
-    showElements,
-    elementEffect,
-    menuColorDetect,
-  } = useContext(MenuListProvider);
-  const [visible, setVisible] = useState(false);
+  const { svgList, mySkillsList, showElements, menuColorDetect } = useContext(MenuListProvider);
+  const [isVisible, currentElement] = useVisibility();
   const aboutSecRef = useRef();
-  const descriptionRef = useRef();
   const animateElContainer = useRef();
   const animateRef = useRef([]);
 
   const tl = gsap.timeline();
 
   useEffect(() => {
-    window.addEventListener("scroll", () =>
-      elementEffect(animateElContainer.current, visible, setVisible)
-    );
-    return () =>
-      window.removeEventListener("scroll", () =>
-        elementEffect(animateElContainer.current, visible, setVisible)
-      );
-  }, []);
-
-  useEffect(() => {
-    if (!animateRef.current || !visible) return;
+    if (!animateRef.current || !isVisible) return;
     tl.add('start', .5)
     .to(animateRef.current, .5, {opacity: 1, stagger: .03, ease: 'linear'}, 'start')
-  }, [visible]);
+  }, [isVisible]);
 
   useEffect(() => {
     const observeElements = Array.from(document.querySelectorAll(".entry"));
@@ -41,12 +25,6 @@ function About() {
   }, []);
 
   const registerEventListeners = () => {
-    window.addEventListener("scroll", () =>
-      elementEffect(descriptionRef.current, visible, setVisible)
-    );
-    window.removeEventListener("scroll", () =>
-      elementEffect(descriptionRef.current, visible, setVisible)
-    );
     window.addEventListener("scroll", () =>
       menuColorDetect(aboutSecRef.current, "about")
     );
@@ -61,9 +39,9 @@ function About() {
         About me
       </h2>
       <div
-        ref={descriptionRef}
+        ref={currentElement}
         className={`entry about__detail flex flex-col gap-4 mb-8 transition-all duration-500 relative opacity-0 left-invisible md:px-12 lg:mx-auto lg:max-w-screen-md xl:mx-0 2xl:max-w-screen-lg xl:transform xl:-translate-x-1/2 ${
-          visible && "opacity-100 !left-0 xl:!left-1/2"
+          isVisible && "opacity-100 !left-0 xl:!left-1/2"
         } -mt-20 xl:!mt-0`}
       >
         <p className="max-w-full leading-7 xl:leading-relaxed">
