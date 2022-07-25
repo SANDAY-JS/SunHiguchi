48f22e818860fcfa23e77b783a283e4a612d1260
SANDAY-JS <san.sh.s3@outlook.jp>
2022-07-25 11:43:31 +0900

    Fixed Animation Timeing

--- src/assets/useVisibility.tsx
+++ src/assets/useVisibility.tsx
@@ -7,7 +7,7 @@ import throttle from "lodash.throttle";
  * @param {number} offset - Number of pixels up to the observable element from the top
  * @param {number} throttleMilliseconds - Throttle observable listener, in ms
  */
-function useVisibility( offset = -200, throttleMilliseconds = 100, afterEffect = false): [Boolean, React.RefObject<any>] {
+function useVisibility( offset = -170, throttleMilliseconds = 100, afterEffect = false): [Boolean, React.RefObject<any>] {
   const [isVisible, setIsVisible] = useState(false);
   const currentElement = useRef<HTMLElement>(null);
   const topMinusOffset = afterEffect ? -1200 : 0;
