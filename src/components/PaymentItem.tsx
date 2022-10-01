cefa035d0810f58dc2d4d5b5f045569e01892c9f
SANDAY-JS <san.sh.s3@outlook.jp>
2022-10-01 17:40:10 +0900

    animated payment

--- src/components/PaymentItem.tsx
+++ src/components/PaymentItem.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
-import useVisibility from "../assets/useVisibility";
+import FadeIn from "../assets/FadeIn";
 
 function PaymentItem({title,matter1,matter2,payment1,payment2,index}) {
-  const [isVisible, currentElement] = useVisibility();
   const [isPageWide, setIsPageWide] = useState(false);
 
-  const colors = ['from-[#50C9C3] to-[#96DEDA] text-mainB', 'from-[#EB3349] to-[#F45C43] text-[#ddd]', 'from-[#4b6cb7] to-[#182848] text-[#ddd]', 'from-[#EDDE5D] to-[#EDDE5D] text-mainB']
+  // const colors = ['from-[#50C9C3] to-[#96DEDA] text-mainB', 'from-[#EB3349] to-[#F45C43] text-[#ddd]', 'from-[#4b6cb7] to-[#182848] text-[#ddd]', 'from-[#EDDE5D] to-[#EDDE5D] text-mainB']
 
   const checkDeviceWidth = () => {
     const match = window.matchMedia("(max-width: 769px)");
@@ -31,44 +30,47 @@ function PaymentItem({title,matter1,matter2,payment1,payment2,index}) {
   }, [isPageWide]);
 
   return (
-    <div ref={currentElement} id={`payment_${title}`}
-      className={`bg-gradient-to-br bg-[rgba(255,255,255,.7)] border border-blue rounded-md py-4 w-full md:w-[45%] 2xl:w-full transition-opacity transform opacity-0 origin-center duration-500
-      relative left-0 overflow-hidden !font-serif ${isVisible && "!opacity-100"}`}>
-      <h3 className="text-2xl font-black text-center">{title}</h3>
-      <table className="w-full border-none bg-opacity-40 mx-2 md:mx-4 lg:mx-6">
-        <thead className="py-4">
-          <tr>
-            <th className="w-1/2 border-none underline p-3 text-left sm:py-4">
-              ご依頼内容
-            </th>
-            <th className="w-1/2 border-collapse underline p-3 text-left sm:py-4">
-              料金
-            </th>
-          </tr>
-        </thead>
+    <FadeIn id={`payment_${title}`} onlyOpacity
+      className={`bg-gradient-to-br bg-[rgba(255,255,255,.7)] border border-blue rounded-md py-4 w-full md:w-[45%] 2xl:w-full origin-center relative left-0 overflow-hidden !font-serif`}>
+        <FadeIn>
+          <h3 className="text-2xl font-black text-center">{title}</h3>
+        </FadeIn>
+      <FadeIn>
+        <table className="w-full border-none bg-opacity-40 mx-2 md:mx-4 lg:mx-6">
+          <thead className="py-4">
+            <tr>
+              <th className="w-1/2 border-none underline p-3 text-left sm:py-4">
+                ご依頼内容
+              </th>
+              <th className="w-1/2 border-collapse underline p-3 text-left sm:py-4">
+                料金
+              </th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr>
-            <td className="w-1/2 border-collapse p-3 sm:py-0">
-              {matter1}
-            </td>
-            <td className="w-1/2 border-collapse text-xl md:text-2xl font-black p-3 sm:py-0">
-              ¥{payment1}～
-            </td>
-          </tr>
-          {matter2 && (
-            <tr className="mt-3">
+          <tbody>
+            <tr>
               <td className="w-1/2 border-collapse p-3 sm:py-0">
-                {matter2}
+                {matter1}
               </td>
               <td className="w-1/2 border-collapse text-xl md:text-2xl font-black p-3 sm:py-0">
-                ¥{payment2}～
+                ¥{payment1}～
               </td>
             </tr>
-          )}
-        </tbody>
-      </table>
-    </div>
+            {matter2 && (
+              <tr className="mt-3">
+                <td className="w-1/2 border-collapse p-3 sm:py-0">
+                  {matter2}
+                </td>
+                <td className="w-1/2 border-collapse text-xl md:text-2xl font-black p-3 sm:py-0">
+                  ¥{payment2}～
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </FadeIn>
+    </FadeIn>
   );
 }
 
